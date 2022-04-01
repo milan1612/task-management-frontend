7636b9fbfee41033d2030c56c46e88b7829bf9b8
Devang Prajapati <devangp@thirdrocktechkno.com>
2022-04-01 18:10:44 +0530

    Updates

---
+++ precache-manifest.e7e242442e1a05a809c3e78119d13c23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f253c58855c989ced4555ec3b639c1d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c816d996b1c48e524261",
+    "url": "/static/css/main.3e7c9450.chunk.css"
+  },
+  {
+    "revision": "f3a18da85cc7413c9489",
+    "url": "/static/js/2.036e3be9.chunk.js"
+  },
+  {
+    "revision": "c816d996b1c48e524261",
+    "url": "/static/js/main.fc39fd9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
