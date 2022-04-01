8295d3f9c20699728741ecc6f59bf9de1cb2644e
Milan Parmar <milanptrt@gmail.com>
2022-04-01 15:29:04 +0530

    changes

---
+++ build/precache-manifest.019642da829fb2f8339b00bed2557834.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae558e23fbf5a6f161e85149fb35947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b835d61854a3ea54c2c3",
+    "url": "/static/css/main.1cc63923.chunk.css"
+  },
+  {
+    "revision": "f35286216a21bd236b2e",
+    "url": "/static/js/2.d9422e75.chunk.js"
+  },
+  {
+    "revision": "b835d61854a3ea54c2c3",
+    "url": "/static/js/main.5bc4ab8d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
