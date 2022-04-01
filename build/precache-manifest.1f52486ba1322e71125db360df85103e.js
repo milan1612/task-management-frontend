31d779b943b64557c63392e460469c0d091c7aa6
Milan Parmar <milanptrt@gmail.com>
2022-04-01 18:16:56 +0530

    update build

---
+++ build/precache-manifest.1f52486ba1322e71125db360df85103e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c953de4c219b1027ff54e854ff36d002",
+    "url": "/task-management-frontend/index.html"
+  },
+  {
+    "revision": "5024b76c425a395eab1c",
+    "url": "/task-management-frontend/static/css/main.1cc63923.chunk.css"
+  },
+  {
+    "revision": "91f516076b6a099bdafe",
+    "url": "/task-management-frontend/static/js/2.c415aeb3.chunk.js"
+  },
+  {
+    "revision": "5024b76c425a395eab1c",
+    "url": "/task-management-frontend/static/js/main.c2b451ce.chunk.js"
+  },
+  {
+    "revision": "170cb679b8a6db4918f5",
+    "url": "/task-management-frontend/static/js/runtime~main.961a32b0.js"
+  }
+]);
